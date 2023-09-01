51379a63fbd3f44164b673ff172152af52cbb839
Przemek Lisowski <przemek_lisowski@o2.pl>
2023-09-01 22:33:50 +0200

    add JS function and carousel

--- src/code.js
+++ src/code.js
@@ -5,3 +5,23 @@ document.addEventListener('click', () => {
         nav.classList.remove('show')
     }
 })
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    for (let i = 0; i < reveals.length; i++) {
+
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 100;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
